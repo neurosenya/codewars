1ce935cf6dc53812cff5cf7605ea7b806244d7f8
neurosenya <mrsena1206@gmail.com>
2020-09-17 17:39:24 +0300

    rewrote it as function

--- last_digit_of_huge_number.py
+++ last_digit_of_huge_number.py
@@ -12,40 +12,45 @@ As we can see, it is possible to determine last digit for a number d^n quite eas
 position of any number in the sequence [1, 2, 3, 4] and by this determine last digit
 of d^n
 '''
-one_period_digits = set([0, 1, 5, 6])
+def last_digit(lst):
+    if not lst:
+        return 1
+    one_period_digits = set([0, 1, 5, 6])
+    two_period_digits = {
+            4: (6, 4),
+            9: (1, 9)
+                        }
+    four_period_digits = {
+            2: (6, 2, 4, 8),
+            3: (1, 3, 4, 7),
+            7: (1, 7, 9, 3),
+            8: (6, 8, 4, 2)
+                          }
+    if len(lst) == 1:
+        return int( str(lst[0])[-1] )
+    while len(lst) > 1:
+        power = lst.pop()
+        number = int( str(lst.pop())[-1] )
+        # determine what is last digit based on modular arithmetic
+        #if power == 0:
+        #    return 1
+        if number in one_period_digits:
+            if power == 0 and number == 0:
+                new_power = 1
+            else: new_power = number
+        elif number in two_period_digits:
+            if power == 1:
+                new_power = number
+            else:
+                print(power)
+                new_power = two_period_digits[number][power % 2]
 
-two_period_digits = {
-        4: (6, 4),
-        9: (1, 9)
-                    }
-four_period_digits = {
-        2: (6, 2, 4, 8),
-        3: (1, 3, 4, 7),
-        7: (1, 7, 9, 3),
-        8: (6, 8, 4, 2)
-                      }
-
-lst = [123232, 694022, 140249]
-
-while len(lst) > 1:
-    power = lst.pop()
-    number = int( str(lst.pop())[-1] )
-    # determine what is last digit based on modular arithmetic 
-    if number in one_period_digits:
-        if number == 0 and power == 0:
-            new_power = 1
+        elif number in four_period_digits:
+            new_power = four_period_digits[number][power % 4]
         else:
-            new_power = number
-    elif number in two_period_digits:
-        new_power = two_period_digits[number][power % 2]
-
-    elif number in four_period_digits:
-        new_power = four_period_digits[number][power % 4]
-    else:
-        new_power = 1
-    lst.append(new_power)
+            new_power = 1
+        lst.append(new_power)
 
 
-print(lst[0])
-
+    return lst[0]
 
