394f8e030bd72fd7d8db1c0a0b897c18aec8d588
neurosenya <mrsena1206@gmail.com>
2020-09-18 22:13:59 +0300

    started playing a little bit with modular arithmetic

--- last_digit_of_huge_number.py
+++ last_digit_of_huge_number.py
@@ -15,25 +15,27 @@ of d^n
 def last_digit(lst):
     if not lst:
         return 1
+    if len(lst) == 1:
+        return int( str(lst[0])[-1] )
     one_period_digits = set([0, 1, 5, 6])
     two_period_digits = {
             4: (6, 4),
             9: (1, 9)
                         }
     four_period_digits = {
-            2: (6, 2, 4, 8),
-            3: (1, 3, 4, 7),
-            7: (1, 7, 9, 3),
-            8: (6, 8, 4, 2)
+            2: (2, 4, 8, 6, 2),
+            3: (3, 9, 7, 1, 3),
+            7: (7, 9, 3, 1, 7),
+            8: (8, 4, 2, 6, 8)
                           }
-    if len(lst) == 1:
-        return int( str(lst[0])[-1] )
+    lst = [int( str(i)[-1] ) for i in lst]
     while len(lst) > 1:
+        print(lst)
         power = lst.pop()
         number = int( str(lst.pop())[-1] )
-        # determine what is last digit based on modular arithmetic
+        # determine what is last digit based on modular arithmetic 
         #if power == 0:
-        #    return 1
+        #    new_power = 1
         if number in one_period_digits:
             if power == 0 and number == 0:
                 new_power = 1
@@ -42,15 +44,24 @@ def last_digit(lst):
             if power == 1:
                 new_power = number
             else:
-                print(power)
-                new_power = two_period_digits[number][power % 2]
-
+                decimal_digit =  power % 10
+                new_power = two_period_digits[number][decimal_digit % 2]
         elif number in four_period_digits:
-            new_power = four_period_digits[number][power % 4]
+            decimal_digit = power % 10
+            new_power = four_period_digits[number][decimal_digit % 5]
         else:
             new_power = 1
         lst.append(new_power)
 
-
     return lst[0]
+
+
+answer = last_digit([123232, 694022, 140249])
+print(answer)
+
+
+
+
+
+
 
