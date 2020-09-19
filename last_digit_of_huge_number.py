376ace230091bd5f59ee182f500adec903a07297
neurosenya <mrsena1206@gmail.com>
2020-09-19 11:40:44 +0300

    completely simplified solution

--- last_digit_of_huge_number.py
+++ last_digit_of_huge_number.py
@@ -18,45 +18,36 @@ def last_digit(lst):
     if len(lst) == 1:
         return int( str(lst[0])[-1] )
     one_period_digits = set([0, 1, 5, 6])
+
     two_period_digits = {
             4: (6, 4),
             9: (1, 9)
                         }
     four_period_digits = {
-            2: (2, 4, 8, 6, 2),
-            3: (3, 9, 7, 1, 3),
-            7: (7, 9, 3, 1, 7),
-            8: (8, 4, 2, 6, 8)
+            2: (6, 2, 4, 8),
+            3: (1, 3, 9, 7),
+            7: (1, 7, 9, 3),
+            8: (6, 8, 4, 2)
                           }
-    lst = [int( str(i)[-1] ) for i in lst]
+    # This part extracts only the required part for further calculations
+    # - 2 last digits
+    lst = [str(i) for i in lst]
+    lst = [int( str(i)[-2:] ) if len(i) > 1 else i for i in lst]
+    # I need to upgrade this part in order to properly calculate
+    # the last digit of a number to some power
+    # for that I need to find the last TWO digits
     while len(lst) > 1:
         print(lst)
-        power = lst.pop()
-        number = int( str(lst.pop())[-1] )
-        # determine what is last digit based on modular arithmetic 
-        #if power == 0:
-        #    new_power = 1
-        if number in one_period_digits:
-            if power == 0 and number == 0:
-                new_power = 1
-            else: new_power = number
-        elif number in two_period_digits:
-            if power == 1:
-                new_power = number
-            else:
-                decimal_digit =  power % 10
-                new_power = two_period_digits[number][decimal_digit % 2]
-        elif number in four_period_digits:
-            decimal_digit = power % 10
-            new_power = four_period_digits[number][decimal_digit % 5]
-        else:
-            new_power = 1
+        power = int( lst.pop() )
+        #number = int( str(lst.pop())[-1] )
+        number = int( lst.pop() )
+        new_power = number**power % 100
         lst.append(new_power)
 
-    return lst[0]
+    return int( str(lst[0])[-1] )
 
 
-answer = last_digit([123232, 694022, 140249])
+answer = last_digit([4, 3, 6])
 print(answer)
 
 
