4bfac4e04040ab9abb0eab51f2df17ede387f6ae
neurosenya <mrsena1206@gmail.com>
2020-09-21 14:25:16 +0300

    improved algorithms, implemented using stack

--- discrete-math/eulers_graph.py
+++ discrete-math/eulers_graph.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 v, e = map(int, input().split())
 graph_adj = [[] for i in range(v)]
@@ -9,10 +10,8 @@ for i in range(e):
      graph_adj[v2 - 1].append(v1-1)
 
 
-graph_adj = [sorted(i, reverse=True) for i in graph_adj]
-def eulers_cycle(graph_adj, e, v):
-    if e == 0 or v == 0:
-        return 'NONE'
+#graph_adj = [sorted(i, reverse=True) for i in graph_adj]
+def connectivity_check(graph_adj, e, v):
     def depth_first_search(graph, current_vertex, visited):
         # mark vertex as visited
         visited.add(current_vertex)
@@ -35,40 +34,50 @@ def eulers_cycle(graph_adj, e, v):
             if len(vertices) == 1:
                 num_components += 1
     if num_components > 1:
+        return None
+    else:
+        return num_components
+
+def even_degree_nodes(graph_adj):
+    for i in graph_adj:
+        if len(i) % 2 != 0 and len(i) != 0:
+            return False
+    return True
+
+def eulers_cycle(graph_adj, v, e):
+    if e==1 and v==1:
+        return 'NONE'
+    if e==0:
+        return 'NONE'
+    if not connectivity_check(graph_adj, v, e):
+        return 'NONE'
+    if not even_degree_nodes(graph_adj):
         return 'NONE'
 
     eulers_cycle = [0]
-    node = 100000
-    for i, n in enumerate(graph_adj):
-        if len(n) < node:
-            node = i
-    #print(graph_adj)
-    for i in graph_adj:
-        if len(i) % 2 != 0:
-            return 'NONE'
-    while len(eulers_cycle) < e: # while cycle did not go through all edges
-        if graph_adj[node]:
-            next_node = graph_adj[node].pop()
-            graph_adj[next_node].remove(node)
-            #print(graph_adj)
-            if graph_adj[next_node]:
-                node = next_node
-                eulers_cycle.append(node)
-            check = [True if i==[] else False for i in graph_adj]
-            if all(check):
-                #print(check)
-                eulers_cycle.append(next_node)
-                break
+    node = random.randint(0, v-1)
+    S = []
+    S.append(node)
+    result = []
+    while S:
+        if graph_adj[S[-1]]:
+            current_node = S[-1]
+            adj_node = graph_adj[S[-1]][-1]
+            S.append(adj_node)
+            graph_adj[current_node].remove(adj_node)
+            graph_adj[adj_node].remove(current_node)
         else:
-            for i in graph_adj:
-                if i:
-                    node = graph_adj.index(i)
-    return [i+1 for i in eulers_cycle]
+            last_node = S.pop()
+            result.append(last_node)
+    if result[0] != result[-1]:
+        return 'NONE'
+    if result:
+        return [i+1 for i in result[:-1]]
 
-answer = eulers_cycle(graph_adj, e, v)
+answer = eulers_cycle(graph_adj, v, e)
+
 if answer == 'NONE':
     print(answer)
 else:
     for i in answer:
         print(i, end=' ')
-
