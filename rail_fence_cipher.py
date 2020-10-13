d3f5d47bebdb96cab202977a76468b5a21c19d76
neurosenya <mrsena1206@gmail.com>
2020-10-13 22:03:51 +0300

    solution from codewars

--- rail_fence_cipher.py
+++ rail_fence_cipher.py
@@ -1,53 +1,67 @@
 from itertools import cycle
 
 def encode_rail_fence_cipher(string, n):
-    if string == '':
-        return ''
-    m = n*2 - 2 # number of element to iterate once through all rails 
-    encoded = string[0]
     rails = ['' for i in range(n)]
-    rails[0] += string[0]
-    for idx in range(m, len(string), m):
-        rails[0] += string[idx]
-        end = idx-1
-        start = idx - (m-1)
-        for rail in range(1, n):
-            if start == end:
-                rails[rail] += string[start]
-            else:
-                rails[rail] += string[start]
-                rails[rail] += string[end]
-            start += 1
-            end -= 1
-    print(rails)
-    return ''.join(i for i in rails)
+    rails_cycle = cycle([i for i in range(n)] + [i for i in range(n-2, 0, -1)])
+    while string:
+        rail = next(rails_cycle)
+        rails[rail] += string[0]
+        string = string[1:]
+    return ''.join(rails)
+
 
 def decode_rail_fence_cipher(string, n):
-    m = n*2 - 2
-    r1 = sum([1 for i in range(0, len(string), m)])-1
-    print(r1)
-    rn = r1
-    rb = r1 * 2
-    print(rb)
-    rails = ['' for i in range(n)]
-    rails[0] = [i for i in string[0:r1+1]]
-    rails[-1] = [i for i in string[-rn:]]
-    start = r1+1
-    for r in range(1, n-1):
-        rails[r] = [i for i in string[start:start+rb]]
-        start += rb
-    print(rails)
-    ping_pong = cycle([i for i in range(n)] + [i for i in range(n-2, 0, -1)])
+    rails = [ '' for i in range(n) ]
+    # no. of symbols in the first rail
+    NoSymbols = len(string)
+    rail1 = sum([1 for i in range(0, len(string), (n*2 - 2))])
+    LeftOverSymbols = NoSymbols - ((rail1 - 1) * 2 * (n - 2)  # no. of symbols in the (2,..,n-2)
+                    + rail1 + (rail1 - 1 )) # no. of symbols in the first and last rail
+    # filling 1st and last rail
+    rails[0] += string[:rail1]
+    # filling the rails (2, ..., n-1) with symbols
+    start = rail1
+    finish = rail1 + 2*(rail1-1)
+    if LeftOverSymbols:
+        NoOfDoubleSymbols = LeftOverSymbols - (n-1)
+        StartDoubles = (n-1) - NoOfDoubleSymbols
+    #print('StartDoubles: %s, LeftOverSymbols: %s' % (StartDoubles, LeftOverSymbols))
+    for i in range(1, n-1):
+        rails[i] += string[start:finish]
+        if LeftOverSymbols > 0:
+            if i < StartDoubles and StartDoubles > 0:
+                rails[i] += string[finish]
+                finish += 1
+                start = finish
+                finish += 2 * (rail1 - 1)
+                LeftOverSymbols -= 1
+            elif i >= StartDoubles and StartDoubles > 0:
+                #print(i)
+                rails[i] += string[finish:finish+2]
+                finish += 2
+                start = finish
+                finish += 2 * (rail1 - 1)
+                LeftOverSymbols -= 2
+        else:
+            start = finish
+            finish += 2 * (rail1 - 1)
+        print(rails)
+    if LeftOverSymbols == 1:
+        LastRail = rail1
+        rails[-1] += string[-LastRail:]
+        LeftOverSymbols -= 1
+    else:
+        rails[-1] = string[-(rail1 - 1):]
     decoded = ''
-    idx = None
-    lst_fst = [0, n-1]
-    idx = next(ping_pong)
-    while len(decoded) < len(string):
-        decoded += rails[idx][0]
-        rails[idx] = rails[idx][1:]
-        idx = next(ping_pong)
-    return decoded
-s = "Hooel,wrdl l"
-#print(decode_rail_fence_cipher(s, 3))
-s1 = "Hello, world"
-print(encode_rail_fence_cipher(s1, 3))
+    rails_cycle = cycle([i for i in range(n)] + [i for i in range(n-2, 0, -1)])
+    while len(decoded) < NoSymbols:
+        rail = next(rails_cycle)
+        decoded += rails[rail][0]
+        rails[rail] = rails[rail][1:]
+    return ''.join(decoded)
+
+
+
+string = "WVOEOETNACRACRSEEEEIDLDF"
+print(len(string))
+print( decode_rail_fence_cipher(string, 6) )
--- rail_fence_cipher2.py
+++ rail_fence_cipher2.py
@@ -1,67 +1,22 @@
-from itertools import cycle
+# Solution from codewars
 
-def encode_rail_fence_cipher(string, n):
-    rails = ['' for i in range(n)]
-    rails_cycle = cycle([i for i in range(n)] + [i for i in range(n-2, 0, -1)])
-    while string:
-        rail = next(rails_cycle)
-        rails[rail] += string[0]
-        string = string[1:]
-    return ''.join(rails)
+from itertools import chain
 
+def fencer(string, n):
+    rails = [ [] for _ in range(n) ]
+    rail, dx = 0, 1
+    for c in string:
+        rails[rail].append(c)
+        if rail == n-1 and dx > 0 or rail == 0 and dx < 0:
+            dx *= -1
+        rail += dx
+    return chain.from_iterable(rails)
 
-def decode_rail_fence_cipher(string, n):
-    rails = [ '' for i in range(n) ]
-    # no. of symbols in the first rail
-    NoSymbols = len(string)
-    rail1 = sum([1 for i in range(0, len(string), (n*2 - 2))])
-    LeftOverSymbols = NoSymbols - ((rail1 - 1) * 2 * (n - 2)  # no. of symbols in the (2,..,n-2)
-                    + rail1 + (rail1 - 1 )) # no. of symbols in the first and last rail
-    # filling 1st and last rail
-    rails[0] += string[:rail1]
-    # filling the rails (2, ..., n-1) with symbols
-    start = rail1
-    finish = rail1 + 2*(rail1-1)
-    if LeftOverSymbols:
-        NoOfDoubleSymbols = LeftOverSymbols - (n-1)
-        StartDoubles = (n-1) - NoOfDoubleSymbols
-    #print('StartDoubles: %s, LeftOverSymbols: %s' % (StartDoubles, LeftOverSymbols))
-    for i in range(1, n-1):
-        rails[i] += string[start:finish]
-        if LeftOverSymbols > 0:
-            if i < StartDoubles and StartDoubles > 0:
-                rails[i] += string[finish]
-                finish += 1
-                start = finish
-                finish += 2 * (rail1 - 1)
-                LeftOverSymbols -= 1
-            elif i >= StartDoubles and StartDoubles > 0:
-                #print(i)
-                rails[i] += string[finish:finish+2]
-                finish += 2
-                start = finish
-                finish += 2 * (rail1 - 1)
-                LeftOverSymbols -= 2
-        else:
-            start = finish
-            finish += 2 * (rail1 - 1)
-        print(rails)
-    if LeftOverSymbols == 1:
-        LastRail = rail1
-        rails[-1] += string[-LastRail:]
-        LeftOverSymbols -= 1
-    else:
-        rails[-1] = string[-(rail1 - 1):]
-    decoded = ''
-    rails_cycle = cycle([i for i in range(n)] + [i for i in range(n-2, 0, -1)])
-    while len(decoded) < NoSymbols:
-        rail = next(rails_cycle)
-        decoded += rails[rail][0]
-        rails[rail] = rails[rail][1:]
-    return ''.join(decoded)
+def encode_rail_fence_cipher(s, n):
+    return ''.join( fencer(s, n) )
 
-
-
-string = "WVOEOETNACRACRSEEEEIDLDF"
-print(len(string))
-print( decode_rail_fence_cipher(string, 6) )
+def decode_rail_fence_cipher(s, n):
+    lst = [''] * len(s)
+    for c, i in zip( s, fencer(range(len(s), n)) ):
+        lst[i] = c
+    return ''.join(lst)
