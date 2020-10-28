87a1558a46215efc3f87125feff8a3d4651c1f95
neurosenya <mrsena1206@gmail.com>
2020-10-28 22:15:04 +0200

    addded commentaries; corrected LiftStops count

--- lift.py
+++ lift.py
@@ -11,22 +11,30 @@ class Dinglemouse:
         ZeroFloor, CurrFloor = 0, 0
         LastFloor = len(self.floors_queues)
         lift = []
-        LiftStops = []
+        LiftStops = [0]
         # empty floors check
         while [True for floor in self.floors_queues
                if any( [isinstance(j, int) for j in floor] ) ]:
-            print(self.floors_queues)
-            # bottom-top
+############################################################################################
+#----------------------------------------- From Top to Bottom --------------------------------------------
             for i in range(ZeroFloor, LastFloor):
+                visited = False
                 CurrFloor = i
-                LiftStops.append(CurrFloor)
-                # take out people
+#------------------------------------------ take out people----------------------------------------------
+                if any([True for person in lift if int(person) == CurrFloor]):
+                    LiftStops.append(CurrFloor)
+                    visited = True
                 for person in lift:
                     if person == CurrFloor:
                         self.floors_queues[CurrFloor].append(str(person))
                 lift[:] = [x for x in lift if x != CurrFloor]
-                # take in people
+#----------------------------------------- take in people--------------------------------------------
                 entered_lift = []
+                # lift stop
+                if any([True for person in self.floors_queues[CurrFloor]
+                    if int(person) > CurrFloor]):
+                    if not visited:
+                        LiftStops.append(CurrFloor)
                 for person in self.floors_queues[CurrFloor]:
                     # full lift condition
                     if len(lift) < self.capacity:
@@ -34,7 +42,8 @@ class Dinglemouse:
                             lift.append(person)
                             entered_lift.append(person)
                     else: break
-                # remove people from the floor
+
+#------------------------------------- remove people from the floor----------------------------------
                 for j in entered_lift:
                     self.floors_queues[CurrFloor].remove(j)
                 # empty lift and floors above
@@ -42,18 +51,25 @@ class Dinglemouse:
                                 if floor]
                 if not lift and not floors_above:
                     break
-                # top-bottom
+##########################################################################################
+#--------------------------------From Top to Bottom---------------------------------------
             print(self.floors_queues)
             for i in range(CurrFloor, ZeroFloor-1, -1):
                 CurrFloor = i
-                LiftStops.append(CurrFloor)
-                # take out people
+                visited = False
+#---------------------------------------take out people -----------------------------------------------
+                if any([True for person in lift if int(person) == CurrFloor]):
+                    LiftStops.append(CurrFloor)
+                    visited = True
                 for person in lift:
                     if person == CurrFloor:
                         self.floors_queues[CurrFloor].append(str(person))
                 lift[:] = [x for x in lift if x != CurrFloor]
-                # take in people
+# ---------------------------------------take in people------------------------------------
                 entered_lift = []
+                if any([True for person in self.floors_queues[CurrFloor] if int(person) < CurrFloor]):
+                    if  not visited:
+                        LiftStops.append(CurrFloor)
                 for person in self.floors_queues[CurrFloor]:
                     # full lift condition
                     if len(lift) < self.capacity:
@@ -61,7 +77,7 @@ class Dinglemouse:
                             lift.append(person)
                             entered_lift.append(person)
                     else: break
-                # remove people from the floor
+#--------------------------------  remove people from the floor -----------------------------------------
                 for j in entered_lift:
                     self.floors_queues[CurrFloor].remove(j)
                 # empty lift and floors below
@@ -70,15 +86,19 @@ class Dinglemouse:
                 if not lift and not floors_below:
                     break
             print(self.floors_queues)
-        LiftStops.extend([x for x in range(CurrFloor, ZeroFloor-1, -1)])
         # remove consecutive duplicate elements
+        LiftStops.append(ZeroFloor)
         LiftStops[:] = [i[0] for i in groupby(LiftStops)]
         return LiftStops
 
 
-queues = [ [], [6, 5, 2], [4], [], [0, 0, 0], [], [], [3, 6, 4, 5, 6], [], [10, 2], [1, 4, 3, 2]] 
-lift = Dinglemouse(queues, 3)
-print(queues)
+test0 = [ [], [6, 5, 2], [4], [], [0, 0, 0], [], [], [3, 6, 4, 5, 6], [], [10, 2], [1, 4, 3, 2]]
+test1 = [ [], [], [5, 5, 5], [], [], [], [] ]
+test2 = [ [], [], [1, 1], [], [], [], []]
+test3 = [ [], [3], [4], [], [5], [], []]
+test4 = [ [], [0], [], [], [2], [3], []]
+lift = Dinglemouse(test0, 3)
+print(test0)
 print(lift.theLift())
 
 
--- lift2.py
+++ lift2.py
@@ -5,7 +5,32 @@ class Dinglemouse:
     def __init__(self, queues, capacity):
         self.floors_queues = [list(floor) for floor in queues]
         self.capacity = capacity
-
+    def onedirection(start_floor):
+       for i in range(ZeroFloor, start_floor):
+           CurrFloor = i
+           LiftStops.append(CurrFloor)
+           # take out people
+           for person in lift:
+               if person == CurrFloor:
+                   self.floors_queues[CurrFloor].append(str(person))
+           lift[:] = [x for x in lift if x != CurrFloor]
+           # take in people
+           entered_lift = []
+           for person in self.floors_queues[CurrFloor]:
+               # full lift condition
+               if len(lift) < self.capacity:
+                   if int(person) > CurrFloor:
+                       lift.append(person)
+                       entered_lift.append(person)
+               else: break
+           # remove people from the floor
+           for j in entered_lift:
+               self.floors_queues[CurrFloor].remove(j)
+           # empty lift and floors above
+           floors_above = [True for floor in self.floors_queues[i+1:]
+                           if floor]
+           if not lift and not floors_above:
+               break
     def theLift(self):
         # check left people on floors
         ZeroFloor, CurrFloor = 0, 0
